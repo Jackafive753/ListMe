c7c60feae1579559804da4cf8cdd1cbb6d292e49
Jackafive <50942349+Jackafive@users.noreply.github.com>
2020-11-14 17:37:21 +0100

    #GuestActivity + GuestActivity.kt um Activtiy zu kontrollieren + guest_activity.xml mit Sidebar und fragment, welches beim navigieren benutzt wird + guest_navigation.xml Init zur Navigation in der GuestActivity * AndroidManifest, GuestActivity als Activity anmelden * build.gradle implementation für navigation und fragmente

---
+++ app/src/main/java/com/hkproductions/listme/guest/GuestActivity.kt
@@ -0,0 +1,12 @@
+package com.hkproductions.listme.guest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hkproductions.listme.R
+
+class GuestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.guest_activity)
+    }
+}
