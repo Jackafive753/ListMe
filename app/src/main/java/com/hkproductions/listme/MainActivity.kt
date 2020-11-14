0d3a5a371c1e6afdbc15931a163866b396f7b324
Jackafive <50942349+Jackafive@users.noreply.github.com>
2020-11-14 19:05:12 +0100

    #MainActivity + start der GuestActivity über Button

--- app/src/main/java/com/hkproductions/listme/MainActivity.kt
+++ app/src/main/java/com/hkproductions/listme/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.hkproductions.listme
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.Window
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.hkproductions.listme.guest.GuestActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_main)
+    }
+
+    fun onGuest(view: View) {
+        startActivity(Intent(this, GuestActivity::class.java))
     }
 }
