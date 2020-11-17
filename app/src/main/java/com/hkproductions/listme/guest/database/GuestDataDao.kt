15634c159cc99f98b93c8991dc9598c82a9d29b6
Jackafive <50942349+Jackafive@users.noreply.github.com>
2020-11-17 19:30:19 +0100

    # Dao * notwendige Änderungen um Fehler zu beheben

--- app/src/main/java/com/hkproductions/listme/guest/database/GuestDataDao.kt
+++ app/src/main/java/com/hkproductions/listme/guest/database/GuestDataDao.kt
@@ -1,7 +1,6 @@
 package com.hkproductions.listme.guest.database
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
@@ -17,14 +16,14 @@ interface GuestDataDao {
     suspend fun update(guest: GuestData)
 
     @Query("SELECT * FROM guest_data_table WHERE guestDataId = :id")
-    suspend fun getDataById(id: Int)
+    suspend fun getDataById(id: Long): List<GuestData>
 
     @Query("SELECT * FROM guest_data_table")
-    suspend fun getAllGuestData(): LiveData<List<GuestData>>
+    fun getAllGuestData(): LiveData<List<GuestData>>
 
     @Query("SELECT * FROM guest_data_table WHERE phone_owner = 1 LIMIT 1")
-    fun getPhoneOwner(): MutableLiveData<GuestData>
+    suspend fun getPhoneOwner(): GuestData?
 
     @Query("SELECT * FROM guest_data_table WHERE phone_owner = 0")
-    suspend fun getHouseMembers(): LiveData<List<GuestData>>
+    fun getHouseMembers(): LiveData<List<GuestData>>
 }
--- app/src/main/java/com/hkproductions/listme/host/database/HostDataDao.kt
+++ app/src/main/java/com/hkproductions/listme/host/database/HostDataDao.kt
@@ -10,36 +10,36 @@ import androidx.room.Update
 interface HostDataDao {
 
     @Insert
-    suspend fun insert(data: HostData)
+    fun insert(data: HostData)
 
     @Update
-    suspend fun update(data: HostData)
+    fun update(data: HostData)
 
     @Query("SELECT * FROM host_data_table WHERE end_time_milli = -1")
-    suspend fun getOpenEntries(): LiveData<List<HostData>>
+    fun getOpenEntries(): LiveData<List<HostData>>
 
     @Query("SELECT * FROM host_data_table")
-    suspend fun getAllEntries(): LiveData<List<HostData>>
+    fun getAllEntries(): LiveData<List<HostData>>
 
-    @Query(
-        "SELECT DISTINCT * FROM host_data_table WHERE " +
-                "first_name+' '+last_name LIKE '%'+:name+'%'"
-    )
-    suspend fun getEntriesByName(name: String): LiveData<List<HostData>>
-
-    @Query(
-        "SELECT DISTINCT * FROM host_data_table WHERE " +
-                "(start_time_milli >= :starttime AND start_time_milli <= :endtime) " +
-                "OR (end_time_milli >= :starttime AND end_time_milli <= :endtime)"
-    )
-    suspend fun getEntriesByTime(starttime: Long, endtime: Long)
-
-    @Query(
-        "SELECT DISTINCT * FROM host_data_table WHERE " +
-                "((start_time_milli >= :starttime AND start_time_milli <= :endtime) " +
-                "OR (end_time_milli >= :starttime AND end_time_milli <= :endtime)) " +
-                "AND first_name+' '+last_name LIKE '%:name%'"
-    )
-    suspend fun getEntriesByNameAndTime(name: String, starttime: Long, endtime: Long)
+//    @Query(
+//        "SELECT DISTINCT * FROM host_data_table WHERE " +
+//                "first_name+' '+last_name LIKE '%'+:name+'%'"
+//    )
+//    fun getEntriesByName(name: String): LiveData<List<HostData>>
+//
+//    @Query(
+//        "SELECT DISTINCT * FROM host_data_table WHERE " +
+//                "(start_time_milli >= :starttime AND start_time_milli <= :endtime) " +
+//                "OR (end_time_milli >= :starttime AND end_time_milli <= :endtime)"
+//    )
+//    fun getEntriesByTime(starttime: Long, endtime: Long)
+//
+//    @Query(
+//        "SELECT DISTINCT * FROM host_data_table WHERE " +
+//                "((start_time_milli >= :starttime AND start_time_milli <= :endtime) " +
+//                "OR (end_time_milli >= :starttime AND end_time_milli <= :endtime)) " +
+//                "AND first_name+' '+last_name LIKE '%:name%'"
+//    )
+//    fun getEntriesByNameAndTime(name: String, starttime: Long, endtime: Long)
 
 }
