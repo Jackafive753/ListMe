83ad17068c82bb2fa86d30781fcc5133e672ebce
Jinglebear <46826783+Jinglebear@users.noreply.github.com>
2020-12-29 16:07:52 +0100

    # com.hkproductions.listme.host.tablemanagement + new package for tablemanagement # TableManagementFragment.java + new class TableManagement # TableManagementViewModel.java + new class TableManagementViewModel # TableManagementViewModelFactory.java + new class TableManagementViewModelFactory

---
+++ app/src/main/java/com/hkproductions/listme/host/tablemanagement/TableManagementViewModelFactory.java
@@ -0,0 +1,13 @@
+package com.hkproductions.listme.host.tablemanagement;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+public class TableManagementViewModelFactory implements ViewModelProvider.Factory {
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return null;
+    }
+}
