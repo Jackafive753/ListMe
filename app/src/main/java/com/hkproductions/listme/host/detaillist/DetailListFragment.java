95773915f45b6e4a23ba4f3c249f58be1570015f
Jinglebear <46826783+Jinglebear@users.noreply.github.com>
2020-12-29 16:04:42 +0100

    # com.hkproductions.listme.host.detaillist + new package for detaillist # DetailListFragment.java + new class DetailListFragment # DetailListViewModel.java + new class DetailListViewModel # DetailListViewModelFactory.java + new class DetailListViewModelFactory

---
+++ app/src/main/java/com/hkproductions/listme/host/detaillist/DetailListFragment.java
@@ -0,0 +1,6 @@
+package com.hkproductions.listme.host.detaillist;
+
+import androidx.fragment.app.Fragment;
+
+public class DetailListFragment extends Fragment {
+}
---
+++ app/src/main/java/com/hkproductions/listme/host/detaillist/DetailListViewModelFactory.java
@@ -0,0 +1,13 @@
+package com.hkproductions.listme.host.detaillist;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+public class DetailListViewModelFactory implements ViewModelProvider.Factory {
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return null;
+    }
+}
