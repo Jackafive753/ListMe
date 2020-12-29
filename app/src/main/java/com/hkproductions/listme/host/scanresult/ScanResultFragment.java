a7d40b3a822eb84ccf41540bc0b1cae06d03d6f4
Jinglebear <46826783+Jinglebear@users.noreply.github.com>
2020-12-29 16:11:23 +0100

    # com.hkproductions.listme.host.scanresult + new package for qr-code scan results # ScanResultFragment.java + new class ScanResultFragment # ScanResultViewModel.java + new class ScanResultViewModel # ScanResultViewModelFactory.java + new class ScanResultViewModelFactory

---
+++ app/src/main/java/com/hkproductions/listme/host/scanresult/ScanResultFragment.java
@@ -0,0 +1,6 @@
+package com.hkproductions.listme.host.scanresult;
+
+import androidx.fragment.app.Fragment;
+
+public class ScanResultFragment extends Fragment {
+}
---
+++ app/src/main/java/com/hkproductions/listme/host/scanresult/ScanResultViewModelFactory.java
@@ -0,0 +1,13 @@
+package com.hkproductions.listme.host.scanresult;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+public class ScanResultViewModelFactory implements ViewModelProvider.Factory {
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return null;
+    }
+}
