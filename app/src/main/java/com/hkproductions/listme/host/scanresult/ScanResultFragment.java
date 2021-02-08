214bbcc64480b26e70c62c16ca706441bca96e8b
hamaximi <51363557+hamaximi@users.noreply.github.com>
2021-02-08 18:47:06 +0100

    #Host_Fragment_Scanresult +Buttons Fertig Functionality +Buttons NewScan Functionality

--- app/src/main/java/com/hkproductions/listme/host/scanresult/ScanResultFragment.java
+++ app/src/main/java/com/hkproductions/listme/host/scanresult/ScanResultFragment.java
@@ -1,6 +1,7 @@
 package com.hkproductions.listme.host.scanresult;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,14 +11,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.hkproductions.listme.R;
 import com.hkproductions.listme.databinding.HostFragmentScanresultBinding;
 import com.hkproductions.listme.host.database.HostDataDao;
 import com.hkproductions.listme.host.database.HostDatabase;
-import com.hkproductions.listme.host.startview.HostStartViewFragmentDirections;
 
 public class ScanResultFragment extends Fragment {
 
@@ -29,7 +31,7 @@ public class ScanResultFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        binding= DataBindingUtil.inflate(inflater,
+        binding = DataBindingUtil.inflate(inflater,
                 R.layout.host_fragment_scanresult,
                 container,
                 false
@@ -43,21 +45,42 @@ public class ScanResultFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         // get Arguments
-        long[] hostDataIds= ScanResultFragmentArgs.fromBundle(requireArguments()).getHostDataIds();
+        long[] hostDataIds = ScanResultFragmentArgs.fromBundle(requireArguments()).getHostDataIds();
         // View Model intialize
-        HostDataDao dataSource= HostDatabase.Companion.getInstance(requireActivity().getApplication()).getHostDataDao();
+        HostDataDao dataSource = HostDatabase.Companion.getInstance(requireActivity().getApplication()).getHostDataDao();
         ScanResultViewModelFactory viewModelFactory = new ScanResultViewModelFactory(dataSource, hostDataIds);
-        viewModel= new ViewModelProvider(this,viewModelFactory).get(ScanResultViewModel.class);
+        viewModel = new ViewModelProvider(this, viewModelFactory).get(ScanResultViewModel.class);
 
-        if(hostDataIds.length==1){
+        if (hostDataIds.length == 1) {
             binding.textviewHostScanresultLabel.setText(getResources().getString(R.string.scannedEntrys_single_text));
-        }else {
+        } else {
             binding.textviewHostScanresultLabel.setText(getResources().getString(R.string.scannedEntrys_multi_text));
         }
-        ScanResultAdapter adap= new ScanResultAdapter();
-        viewModel.getLData().observe(getViewLifecycleOwner(),list -> adap.setData(list));
+        ScanResultAdapter adap = new ScanResultAdapter();
+        viewModel.getLData().observe(getViewLifecycleOwner(), adap::setData);
         binding.recyclerviewHost.setAdapter(adap);
 
+        binding.buttonHostScanResultContinue.setOnClickListener(event -> Navigation.findNavController(requireView()).navigate(ScanResultFragmentDirections.actionScanResultToHostStartView()));
 
+        binding.buttonHostScanresultNewScan.setOnClickListener(event -> {
+            IntentIntegrator integrator = IntentIntegrator.forSupportFragment(this);
+            integrator.setDesiredBarcodeFormats(IntentIntegrator.DATA_MATRIX, IntentIntegrator.QR_CODE);
+            integrator.setPrompt(getResources().getString(R.string.host_scan_header));
+            integrator.setOrientationLocked(true);
+            integrator.initiateScan();
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() != null) {
+                viewModel.scannedCode(result.getContents(), requireContext());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
--- app/src/main/java/com/hkproductions/listme/host/scanresult/ScanResultViewModel.kt
+++ app/src/main/java/com/hkproductions/listme/host/scanresult/ScanResultViewModel.kt
@@ -1,21 +1,41 @@
 package com.hkproductions.listme.host.scanresult
 
+import android.content.Context
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.hkproductions.listme.R
+import com.hkproductions.listme.host.checkinout
 import com.hkproductions.listme.host.database.HostData
 import com.hkproductions.listme.host.database.HostDataDao
 import kotlinx.coroutines.launch
 
-class ScanResultViewModel(val database:HostDataDao, val hostDataIds:LongArray) : ViewModel(){
+class ScanResultViewModel(val database:HostDataDao, val hostDataIds:LongArray) : ViewModel() {
     val lData = MutableLiveData<List<HostData>>()
+
     init {
         viewModelScope.launch {
-            val nData = mutableListOf<HostData>()
-            for (max in hostDataIds){
-                nData.add(database.getHostDataById(max))
-            }
-            lData.value=nData.toList()
+            makingList(hostDataIds)
         }
+    }
+
+    fun scannedCode(bao: String, context: Context) {
+        viewModelScope.launch {
+            try {
+                var erg = checkinout(bao, database)
+                makingList(erg)
+            } catch (e: NoSuchElementException) {
+                Toast.makeText(context, R.string.scan_failure_error_text, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    suspend fun makingList(boa: LongArray) {
+        val nData = mutableListOf<HostData>()
+        for (max in boa) {
+            nData.add(database.getHostDataById(max))
+        }
+        lData.value = nData.toList()
     }
 }
