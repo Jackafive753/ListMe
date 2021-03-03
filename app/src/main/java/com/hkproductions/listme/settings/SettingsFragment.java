a437e2594bbc26ce5927e72ea5ae602b606cf376
Jinglebear <46826783+Jinglebear@users.noreply.github.com>
2021-03-03 18:23:09 +0100

    # SettingsFragment.java + new Method initSwitchRememberDecision, that initiates the switch with the value from the sp file and listenes to changes and writes into the file if changes were made

--- app/src/main/java/com/hkproductions/listme/settings/SettingsFragment.java
+++ app/src/main/java/com/hkproductions/listme/settings/SettingsFragment.java
@@ -3,6 +3,8 @@ package com.hkproductions.listme.settings;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,34 +14,53 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.hkproductions.listme.databinding.SettingsFragmentBinding;
-import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.hkproductions.listme.R;
 
 public class SettingsFragment extends Fragment {
 
     private SettingsFragmentBinding binding;
     private SettingsViewModel viewModel;
+    private static final String PREFS_LISTME = "com.hkproductions.listme";
+    private SharedPreferences sp;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
 
-        binding = DataBindingUtil.inflate(inflater,R.layout.settings_fragment,container,false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.settings_fragment, container, false);
         ViewModelProvider viewModelProvider = new ViewModelProvider(this);
         viewModel = viewModelProvider.get(SettingsViewModel.class);
 
+        initSwitchRememberDecision();
 
 
         return binding.getRoot();
 
 
     }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-    binding.setViewModel(viewModel);
-    binding.setLifecycleOwner(this);
+        super.onViewCreated(view, savedInstanceState);
+        sp = getContext().getSharedPreferences(PREFS_LISTME, Context.MODE_PRIVATE);
+
+        binding.setViewModel(viewModel);
+        binding.setLifecycleOwner(this.getViewLifecycleOwner());
     }
 
+    /**
+     * This Method initiates the switch on the Settings Fragment
+     * The Switch sets the value of the decisionRememberDecision boolean in the shared preferences file to
+     * either true or false
+     * A TRUE state represents the user does not want to get asked every time he checks someone out (switch is checked OFF)
+     * A FALSE state represents the user wants to get asked every time he checks someone out (switch is checked ON )
+     */
+    private void initSwitchRememberDecision(){
+        binding.switchMaterial.setUseMaterialThemeColors(true);
+        binding.switchMaterial.setChecked(!sp.getBoolean("decisionRememberDecision", true));
+        sp.edit().putBoolean("decisionRememberDecision", !binding.switchMaterial.isChecked()).apply();
+    }
 }
